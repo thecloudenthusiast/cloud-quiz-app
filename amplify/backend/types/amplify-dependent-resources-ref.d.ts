7bed80495f9afb66f9f1b89a9535d52eae892a10
thecloudenthusiast <dekoonlay4real@gmail.com>
2024-06-19 00:48:03 -0400

    added all the files in initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cloudquizapp3f20cc34": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/quizData.js
@@ -0,0 +1,55 @@
+const quizData = [
+    {
+      question: "Which AWS service is commonly used for file storage?",
+      options: ["AWS Lambda", "Amazon EC2", "Amazon S3", "AWS SQS"],
+      answer: "Amazon S3"
+    },
+    {
+      question: "Which of the following is NOT a type of cloud deployment model?",
+      options: ["Public Cloud", "Private Cloud", "Hybrid Cloud", "Solo Cloud"],
+      answer: "Solo Cloud"
+    },
+    {
+      question: "Which is NOT a cloud service provider in The US?",
+      options: ["Amazon Web Services(AWS)", "Microsoft Azure", "Google Cloud", "Scaleway"],
+      answer: "Scaleway"
+    },
+    {
+      question: "Which is an example of Infrastructure as Code (IaC) tool?",
+      options: ["Terraform", "JSON", "API", "Kubernetes"],
+      answer: "Terraform"
+    },
+    {
+      question: "What does 'SMB' stand for?",
+      options: ["Small Message Block", "Server Message Block", "Standard Mail Box", "Standard Message Block"],
+      answer: "Server Message Block"
+    },
+    {
+      question: "Which service model in cloud computing allows users to rent virtual machines?",
+      options: ["SaaS", "VaaS", "PaaS", "IaaS"],
+      answer: "IaaS"
+    },
+    {
+      question: "What does 'API' stand for?",
+      options: ["Application Port Interface", "Analog Protocol Index", "Application Programming Interface", "Analog Programing Index"],
+      answer: "Application Programming Interface"
+    },
+    {
+      question: "What was Google's original name?",
+      options: ["BackRub", "Googol", "SearchMaster", "WebSearch"],
+      answer: "BackRub"
+    },
+    {
+      question: "What does 'GPU' stand for?",
+      options: ["Graphical Processing Unit", "Graphics Performance Unit", "Graphics Processing Unit", "Graphical Performance Unit"],
+      answer: "Graphics Processing Unit"
+    },  
+    {
+      question: "What does 'IAM' stand for?",
+      options: ["Internet Access Monitor", "Identity & Access Management", "Identity Access Monitor", "Internet Access Management"],
+      answer: "Identity & Access Management"
+    }
+  ];
+  
+  export default quizData;
+  
